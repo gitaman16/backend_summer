25fe0025f9c554debc9b99ee4446a37090dd5ac7
gitaman16 <2023ucp1707@mnit.ac.in>
2026-06-09 09:31:40 +0530

    basic setup

---
+++ src/db/index.js
@@ -0,0 +1,18 @@
+import { configDotenv } from "dotenv";
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log("\nDB connected : ", connectionInstance.connection.host);
+
+    } catch (error) {
+        console.error("DB connection failed, error : ", error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
---
+++ src/index.js
@@ -0,0 +1,32 @@
+import 'dotenv/config'
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import connectDB from './db/index.js';
+
+connectDB()
+.then(app.listen(process.env.PORT || 8080, (req, res) => {
+    console.log(`\nServer started on Port : ${process.env.PORT}`);
+}))
+.catch((err) => {
+    console.log("\nError while connecting DB!! : ", err);
+})
+
+
+
+
+
+
+
+
+
+
+
+// (async () => {
+//     try {
+//         const connectionInstance = await  mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//         console.log(`DB connected : ${connectionInstance}`);
+//     } catch (error) {
+//         console.error(`Failed to connect to DB : ${error}`);
+//         throw error;
+//     }
+// })()
